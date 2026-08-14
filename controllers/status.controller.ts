6a7515c2514f21a3b35094d0bd79aaf8c72d7ff8
vickynot-10 <brianvickyx@gamil.com>
2026-08-14 23:14:39 +0530

    defaulr status

--- controllers/status.controller.ts
+++ controllers/status.controller.ts
@@ -5,6 +5,7 @@ import { ObjectId } from "mongodb";
 import { StatusSchema } from "../schema/status.schema";
 export async function GetStatus(req: FastifyRequest, reply: FastifyReply) {
   try {
+    const { search } = req.query as any;
     const { user_id } = req.user;
 
     if (!user_id || !ObjectId.isValid(user_id)) {
@@ -13,12 +14,20 @@ export async function GetStatus(req: FastifyRequest, reply: FastifyReply) {
 
     const db = get_db();
 
+    const match: any = {
+      fk_user_id: new ObjectId(user_id),
+    };
+
+    if (search) {
+      match.name = {
+        $regex: search,
+        $options: "i",
+      };
+    }
+
     const data = await db
       .collection("status")
-      .find(
-        { fk_user_id: new ObjectId(user_id) },
-        { projection: { fk_user_id: 0 } },
-      )
+      .find(match, { projection: { fk_user_id: 0 } })
       .toArray();
     return send_success(reply, data, 200);
   } catch (err) {
@@ -97,16 +106,16 @@ export async function DeleteStatus(req: FastifyRequest, reply: FastifyReply) {
       return send_error(reply, "Unauthorized", 401);
     }
 
-
-    const  ids  = req.body as string[]
+    const ids = req.body as string[];
 
     if (!ids || ids.length <= 0) {
       return send_error(reply, "Invalid Payload ", 400);
     }
 
-    const filter_ids: ObjectId[] = ids.filter(id => ObjectId.isValid(id)).map(id => new ObjectId(id))
+    const filter_ids: ObjectId[] = ids
+      .filter((id) => ObjectId.isValid(id))
+      .map((id) => new ObjectId(id));
 
-    
     if (filter_ids.length <= 0) {
       return send_error(reply, "Invalid Payload ", 400);
     }
@@ -114,17 +123,75 @@ export async function DeleteStatus(req: FastifyRequest, reply: FastifyReply) {
     const db = get_db();
 
     const delete_result = await db.collection("status").deleteMany({
-      _id : {$in : filter_ids},
-      fk_user_id : new ObjectId(user_id)
-    })
+      _id: { $in: filter_ids },
+      fk_user_id: new ObjectId(user_id),
+    });
 
-    
     if (delete_result.deletedCount === 0) {
       return send_error(reply, "Nothing deleted", 404);
     }
 
-
     return send_success(reply, {}, 200, "Deleted Successfully !");
+  } catch (err) {
+    return send_error(reply, "Internal Server Error", 500);
+  }
+}
+
+export async function SetDefaultStatus(
+  req: FastifyRequest,
+  reply: FastifyReply,
+) {
+  try {
+    const { user_id } = req.user;
+
+    if (!user_id || !ObjectId.isValid(user_id)) {
+      return send_error(reply, "Unauthorized", 401);
+    }
+    const { id } = req.body as any;
+
+    if (!id || !ObjectId.isValid(id)) {
+      return send_error(reply, "Invalid Status", 400);
+    }
+    const db = get_db();
+    const obj_id = new ObjectId(id);
+    const user_obj_id = new ObjectId(user_id);
+    const [update, _] = await Promise.all([
+      db.collection("status").updateOne(
+        {
+          _id: obj_id,
+          fk_user_id: user_obj_id,
+        },
+        {
+          $set: {
+            default: true,
+            updated_on: new Date(),
+          },
+        },
+      ),
+      db.collection("status").updateMany(
+        {
+          _id: {
+            $ne: obj_id,
+          },
+          fk_user_id: user_obj_id,
+        },
+        {
+          $set: {
+            default: false,
+          },
+        },
+      ),
+    ]);
+
+    if (!update || !update.acknowledged) {
+      return send_error(reply, "Internal Server Error", 500);
+    }
+
+    if (update.matchedCount === 0) {
+      return send_error(reply, "Nothing Updated", 404);
+    }
+
+    return send_success(reply, {}, 200, "Updated Successfully !");
   } catch (err) {
     return send_error(reply, "Internal Server Error", 500);
   }
--- routes/status.route.ts
+++ routes/status.route.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { SaveStatus , GetStatus, DeleteStatus } from "../controllers/status.controller";
+import { SaveStatus , GetStatus, DeleteStatus, SetDefaultStatus } from "../controllers/status.controller";
 export async function StatusRoutes(app: FastifyInstance) {
   app.get("/", GetStatus);
   app.post("/", SaveStatus);
   app.delete("/", DeleteStatus);
+  app.patch("/", SetDefaultStatus);
 }
